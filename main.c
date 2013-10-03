abe7ad67f4acd2dcdc872a7edaf30add7e3c8865
amiettinen <amiettinen@colgate.edu>
2013-10-03 14:46:05 -0400

    Added while loops and mode parsing

--- main.c
+++ main.c
@@ -13,14 +13,57 @@
 
 
 int main(int argc, char **argv) {
-  printf("Changes made");
+  printf("\n This is a prompt. Type 'exit' to get out! \n mode input is 'sequential', 's'  or 'parallel', 'p'  \n");
+  prog = stdin;
 
 
+  const char* delim = "\n\t ;";
+  int mode = 0; // default to sequential 
+   
+  char* new_line = (char * ) malloc(sizeof(char)*1024);
+  new_line =  fgets( new_line, 1024 ,  prog);
 
-    printf("main.c, so lonely.  no code here yet :-(\n");
+  while (1) //We always read in a new line.
+    {
+      char * tmp= NULL;
+      char * new_parse = strtok_r( new_line, delim, &tmp);
+
+      while(1){ //always parse. I guess breaks will happen manually so far
+	// as new job come in, check the parse for exit or mode
 
 
+	if(strcmp(new_parse, "mode") == 0)
+	  {
+	    new_parse = strtok_r( new_line, delim, &tmp);
+	    if(new_parse[0]=='s'){
+	      mode = 0;
+	      new_parse = strtok_r( new_line, delim, &tmp);
+	    };
+	    else(){
+		mode = 1;
+		new_parse = strtok_r( new_line, delim, &tmp);
+	      };
+	    continue; // run while loop again
+	  };
 
+
+	if(strcmp(new_parse,"exit") == 0){
+	    // DOSTUFF and break;break;
+	  };
+
+
+      }
+      if(strcmp(new_parse) == 0 ){
+	//here, finish all jobs, and then exit program
+
+      };
+
+    }
+
+
+  fclose(prog);
+
+  printf("\n You entered the word 'exit'. Peace out, yo!")
     return 0;
 }
 
